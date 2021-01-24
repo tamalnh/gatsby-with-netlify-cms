2c38a4aa827e700a269c6fd1d4f635434f947f0d
Tamal H <tamal.nh@gmail.com>
2021-01-24 23:07:26 +0600

    -_-

--- gatsby-config.js
+++ gatsby-config.js
@@ -72,7 +72,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-netlify-cms`,
       options: {
-        // enableIdentityWidget: true,
+        enableIdentityWidget: true,
       }
     },
     `gatsby-plugin-react-helmet`, 
